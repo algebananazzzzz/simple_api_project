782b82958d79ca05eff57880889a5fb920b99b0b
algebananazzzzz.devops <algebananazzzzz.devops@gmail.com>
2024-02-28 01:47:03 +0800

    🧪 test: added ls

--- src/main.go
+++ src/main.go
@@ -2,21 +2,13 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"net/http"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-func handler(ctx context.Context) (*string, error) {
-	output := "Hello v1"
-	fmt.Print(output)
-
-	return &output, nil
-}
-
-func LambdaHandler(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+func handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	return events.APIGatewayProxyResponse{
 		StatusCode: http.StatusOK,
 		Body:       "Hello World!",
